f29acf038733971659d3d3924b934868bde03d26
mkdoherty <m.doherty.7@gmail.com>
2017-01-18 13:26:37 -0500

    gaminder R code'

--- gapminder_fun.R
+++ gapminder_fun.R
@@ -1,4 +1,5 @@
-#Header and shit
+#Header and shit and shit
+
 
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
