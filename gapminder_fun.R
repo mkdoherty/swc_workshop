28fcf9eeeda2154c10f812811406b69dfbda1145
mkdoherty <m.doherty.7@gmail.com>
2017-01-18 11:22:02 -0500

    header and shit

--- gapminder_fun.R
+++ gapminder_fun.R
@@ -1,10 +1,8 @@
+#Header and shit
+
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
-head(gapminder
-		 )
-download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
-gapminder <- read.csv("gapminder-FiveYearData.csv")
-head(gapminder
+head(gapminder)
 colnames(gapminder)
 str(gapminder)
 is_long_lifeExp <- gapminder$lifeExp > 80
